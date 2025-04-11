577e923d131280d57355f3f873cd2281adce2c14
Kevin <kevin@correo.es>
2025-04-11 15:13:08 +0200

    Login form structure, useForm Hook created and imported

--- src/Auth/Pages/HomePage.jsx
+++ src/Auth/Pages/HomePage.jsx
@@ -1,1 +1,18 @@
 //esto va a ser la Home Page sin loguearse.
+import { LoginForm } from "../Components/LoginForm";
+
+export const HomePage = () => {
+  
+    return (
+      <>
+      <header>
+          Esto es HEADER
+      </header>
+  
+      <body>
+          <LoginForm />
+      </body>
+  
+      </>
+    )
+  }
